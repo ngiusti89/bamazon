1c3ab970bb09a433eb36ddacf6f8334cfd3809ae
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-05-18 16:43:43 -0700

    mysql database connection working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,26 @@
+// dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// connection to database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+  });
+
+  function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  }
