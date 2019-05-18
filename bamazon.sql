64b9db7e5eb5cc120142263009be3156ce0a1e92
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-05-18 16:31:17 -0700

    data added to table

--- bamazon.sql
+++ bamazon.sql
@@ -8,8 +8,22 @@ CREATE TABLE products(
   product_name VARCHAR(100) NOT NULL,
   dapartment_name VARCHAR(30) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
-  stock_quantity INT(50),
+  stock_quantity INT(100),
   PRIMARY KEY (item_id)
 );
 
 SELECT * FROM products;
+
+INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
+VALUES ("Baseball Hats", "Apparel", 9.99, 20),
+("Bose Headphones", "Electronics", 250.00, 5),
+("Tie-Dye Shirts", "Apparel", 6.99, 10),
+("USB Hub", "Electronics", 49.99, 20),
+("Retractable Dog Leash", "Pets", 12.99, 15),
+("Tennis Balls", "Sports", 3.99, 12),
+("Womens Lacrosse Stick", "Sports", 200.00, 5),
+("Office Chair", "Home", 150, 2),
+("Dove Shampoo", "Cosmetics", 8.50, 10),
+("XBox One", "Electronics", 299.99, 5);
+
+SELECT * FROM products
