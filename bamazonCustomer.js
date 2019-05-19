77d7ac2c2c7b6798eaff90abc066168b3c809d3c
Natasha Giusti <natashagiusti@Tashs-Mac.local>
2019-05-18 19:54:35 -0700

    inquirer works but with errors

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,16 +11,53 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
+    console.log("Welcome to bAmazon! You are now connected to the bAmazon Store as id " + connection.threadId);
+    console.log('------------------------------------------------------------------------')
+    inventory();
+});
 
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
+function inventory() {
+    console.log("ID |  Product Name |  Department Name |  Price  | Stock Quantity")
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].dapartment_name + " | $" + res[i].price + " | " + res[i].stock_quantity)
+
+        }
+        // console.log(res)
     });
-  }
+    purchase();
+    // connection.end();
+};
+
+function purchase() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item_id",
+            message: "What is the Item ID of the product you would like to buy?"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many do you need?"
+        }
+    ]).then(function (answer) {
+        var item = answer.item_id;
+        var quantityNeeded = answer.quantity;
+
+        connection.query("SELECT * FROM products WHERE item_id " + item, function (err, res) {
+            if (err) throw err;
+            if (res[0].stock_quantity - quantityNeeded >= 0) {
+                var totalCost = res[0].price * quantityNeeded;
+                console.log("Great news! We got it!")
+                console.log(quantityNeeded + " " + res[0].product_name + " will cost " + totalCost);
+                connection.query("UPDATE products SET stock_quantity = " + (res[0].stock_quantity - quantityNeeded) + "WHERE item_id =" + item);
+            } else {
+                console.log("Insufficient quantity")
+            };
+        });
+    });
+};
